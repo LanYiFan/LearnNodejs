7782f2a52971ead45fb07803e4492a974e3eff63
LanYiFan <30793345+LanYiFan@users.noreply.github.com>
2017-08-14 17:33:29 +0800

    081401

---
+++ TestNode/lib/chat_server.js
@@ -0,0 +1,23 @@
+var socketio=require('socket.io');
+var io;
+var guestNumber=1;
+var nickNames={};
+var nameUsed={};
+var currentRoom={};
+exports.listen=function (server) {
+    io=socketio.listen(server);
+    io.set('log level',1);
+    io.sockets.on('ocnnection',function (socket) {
+        guestNumber=assignGuestName(socket,guestNumber,nickNames,nameUsed);
+        joinRoom(socket,'Lobby');
+        handleMessageBroadcasting(socket,nickNames);
+        handleNameChangeAttempts(socket,nickNames,nameUsed);
+        handleRoomJoining(socket);
+        socket.on('rooms',function () {
+            socket.emit('rooms',io.sockets.manager.rooms)
+        });
+        handleClientDisconnection(socket,nickNames,namesUsed);
+    })
+}
+
+
